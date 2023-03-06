d07e9bca9d8501b00fc72c33c5a913badfe2b0ab
Johannesh Haertel <johanneshaertel@uni-koblenz.de>
2023-03-06 10:07:30 +0100

    help parsing java

---
+++ lab6/SomeClassDoingNothing.java
@@ -0,0 +1,14 @@
+package org.softlang.example;
+
+public class SomeClassDoingNothing {
+
+    public static void main(String[] args) {
+        if (true) {
+            doSomthing();
+        }
+    }
+
+    public static doSomthing() {
+
+    }
+}
